5fef1e63f0eab2e5edb8b20d0478e2570ed28d45
Yorkyat <Yorkyat@users.noreply.github.com>
2018-03-18 17:02:06 +0800

    Add: save hostname

---
+++ scripts/popup.js
@@ -0,0 +1,11 @@
+
+$(document).on("click", "#save", function(){
+    event.preventDefault();
+    hostname = $("#hostname").val();
+    chrome.storage.local.set({"hostname": hostname}, function() {
+        console.log('Value is set to ' + hostname);
+    });
+    chrome.storage.local.get(["hostname"], function(result) {
+        console.log('Value currently is ' + result.hostname);
+    });
+});
